ed3d6bb114784097ded4574228a06bfab1dcece9
Jonathan Hogg <jonnty+git@gmail.com>
2021-12-06 00:49:08 +0000

    Nice active nav link treatment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { BrowserRouter as Router, Switch, Route, Link, useParams, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams, useHistory, NavLink } from 'react-router-dom';
 import usePromise from 'react-use-promise';
 import { DayMap, importDays } from './utils';
 
@@ -11,12 +11,14 @@ interface HeaderProps {
 export function Header({ visibleDays }: HeaderProps) {
   return (
     <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <Link to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <Link
+        to="/"
+        className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         <strong>AoC</strong>
       </Link>
 
-      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        {visibleDays.map(n => <li><Link to={`/day/${n}`} className="nav-link px-2 link-dark">Day {n}</Link></li>)}
+      <ul className="nav nav-pills col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        {visibleDays.map(n => <li><NavLink to={`/day/${n}`} activeClassName="nav-link px-2 link-dark active" className="nav-link px-2 link-dark">Day {n}</NavLink></li>)}
       </ul>
       <div className="col-md-3 text-end">
       </div>
